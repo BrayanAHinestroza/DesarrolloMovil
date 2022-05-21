3d2e38d8567d96d086dd396a42f812f2c579fc6a
Santiago Marmolejo <santiagomc2k16@gmail.com>
2022-05-20 22:20:43 -0500

    maps

--- app/src/main/java/com/example/proyectofinal/EventsMapBox.java
+++ app/src/main/java/com/example/proyectofinal/EventsMapBox.java
@@ -3,18 +3,36 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+
 
 public class EventsMapBox extends AppCompatActivity {
+    MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_map_box);
 
+
         Intent myIntent = getIntent();
         String latitud = myIntent.getStringExtra("lat");
         String longitud = myIntent.getStringExtra("long");
 
+        Uri gmmIntentUri = Uri.parse("google.streetview:cbll="+latitud+","+longitud);
+
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        startActivity(mapIntent);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
 
     }
 }
